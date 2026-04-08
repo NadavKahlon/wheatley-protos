6c822496071ad59c5c70a17400bc4551d241524b
Nadav Kahlon <nadavkahlon100@gmail.com>
2026-04-08 05:05:41 +0300

    system: seperate protos into a dedicated repo

--- agent/WheatleyProto/include/WheatleyProto.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#pragma warning(push, 0)
-#include <agent\command.pb.h>
-#include <stream.pb.h>
-#pragma warning(pop)
--- server/src/wheatley_server/utils.py
+++
@@ -1,2 +0,0 @@
-def snake_to_pascal(text):
-    return "".join(word.capitalize() for word in text.split("_"))
